20c0870e365def50795c30170c4aab3345dd1829
Sahas Suraweera <sahassuraweera11@gmail.com>
2025-11-03 01:06:28 +0530

    feat: add SQLite database connection and initial backend setup

--- Server/server.js
+++ Server/server.js
@@ -10,4 +10,14 @@ app.get('/', (req, res) => {
   res.send('Backend is working!');
 });
 
+
+app.get('/userInfo', (req, res) => {
+  const sampleUsers = [
+    { user_id: 1, firstName: 'Sahan', lastName: 'Fernando', email: 'sahan.f1@gmail.com' },
+    { user_id: 2, firstName: 'Kavindi', lastName: 'Jayasinghe', email: 'kavindi.j1@gmail.com' },
+    { user_id: 2, firstName: 'Kavindi', lastName: 'Jayasinghe', email: 'kavindi.j1@gmail.com' }
+  ];
+  res.json(sampleUsers);
+});
+
 app.listen(5000, () => console.log('Server running on port 5000'));
